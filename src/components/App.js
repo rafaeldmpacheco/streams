2549c01e9eacd6338f247dcd0d04174adf0af4d8
Rafael Pacheco <rafael.pacheco@senior.com.br>
2019-01-30 10:59:14 -0200

    add react router

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>
+      PageOne
+      <br />
+      <Link to="/pageTwo">Navigate to Page Two</Link>
+    </div>;
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <br />
+      <button>Click Me!</button>
+      <br />
+      <Link to="/">Navigate to Page One</Link>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
